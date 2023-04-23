004f29d93e2d283ac96beb9e52e8eb1c511a1d4a
Jose Dominguez <jdomindev@gmail.com>
2023-04-23 19:29:40 -0400

    remove addOrder mutation from success page

--- client/src/components/pages/Success.js
+++ client/src/components/pages/Success.js
@@ -1,23 +1,14 @@
 import React, { useEffect } from "react";
-import { useMutation } from "@apollo/client";
-
-import { ADD_ORDER } from "../../utils/mutations";
 
 // import "../assets/success.css";
 
 const Success = (props) => {
-  const [addOrder] = useMutation(ADD_ORDER);
   const { cartItems } = props;
 
   useEffect(() => {
     async function saveOrder() {
-      const cart = await JSON.parse(localStorage.getItem("cartItems"));
-      const products = cart.map((item) => item._id);
-  
-      // Need to find a way to make sure quantities get passed on here
-      if (products.length) {
-        await addOrder({ variables: { products } });
-        
+    
+      if (cartItems.length) {
         cartItems.forEach((index, object) => {
             cartItems.splice(index);
         });
@@ -31,7 +22,7 @@ const Success = (props) => {
     }
 
     saveOrder();
-  }, [addOrder, cartItems]);
+  }, [cartItems]);
 
   return (
     <div className="d-flex justify-content-center mt-5">
