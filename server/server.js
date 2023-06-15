5240bc295ed00c3a255e23f9376b390ae488f53c
Jose Dominguez <jdomindev@gmail.com>
2023-06-15 12:44:10 -0400

    adds needed changes to display product images in stripe checkout

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ server.applyMiddleware({ app });
 var whitelist = [
   "https://ecommerce-react-backend.onrender.com",
   "https://ecommerce-react-backend.onrender.com/graphql",
+  "https://d2dre0lzik6pal.cloudfront.net"
 ];
 
 var corsOptions = {
